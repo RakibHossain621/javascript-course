a22e551483da346fc0d18309e51ee2dc73f8f784
RakibHossain621 <117344777+RakibHossain621@users.noreply.github.com>
2024-05-28 06:57:37 +0000

    push

---
+++ basic/variable.js
@@ -0,0 +1,9 @@
+
+var  names = "Rakib Hossian"
+var age = 24
+// console.log(name)
+// console.log(age)
+
+console.log(names + " knows javasecript")
+console.log("His age only " + age)
+console.log(names + " is a creating a java secript for all")
