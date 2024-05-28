2457be1227cd212bc2824800a8883c6bda23e4e0
RakibHossain621 <117344777+RakibHossain621@users.noreply.github.com>
2024-05-28 09:21:36 +0000

    push

---
+++ basic/typeconverson.js
@@ -0,0 +1,12 @@
+var str = '1000'
+var n = 10
+// console.log(Number.parseFloat(str))
+// console.log(n.toString(n))
+// console.log(Boolean(-Infinity))
+
+console.log(Boolean(""))
+console.log(Boolean("sssssss"))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean(0))
+console.log(Boolean(451))
