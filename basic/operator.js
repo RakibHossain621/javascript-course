a68aca85dee2d1ba2f8f6e2e0ebeca01ba72bccf
RakibHossain621 <117344777+RakibHossain621@users.noreply.github.com>
2024-05-28 10:31:26 +0000

    push

---
+++ basic/operator.js
@@ -0,0 +1,47 @@
+// arithmetic operator
+
+// + - * / % ++ --
+
+// var a = 11
+// var b = 6
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// // console.log(a++)
+// // console.log(a--)
+// console.log(++a)
+// console.log(--a)
+
+// assignment operator//
+// var a = 20
+// var b = 10
+//  a += b
+//  console.log(a)
+// a -= b
+//  console.log(a)
+//  a *= b
+//  console.log(a)
+
+// comparison opertaor//
+ var a = 10
+ var b = 20
+ console.log(a == b)  // fasle
+ console.log(a != b)  // true
+ console.log(a > b) // fasle
+ console.log(a <b)  // true
+
+ a = 20
+ console.log(a <= b) // true
+ console.log(a >= b)  // true
+
+ var c = "50"
+ var d = 50
+ console.log(c === d)
+ console.log(c !== d)
+
+//  logical operator
+// &&
+// !
+// ||
