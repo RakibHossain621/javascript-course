80e47a89da1376d95bd25d0551a0e181c1c066e3
RakibHossain621 <117344777+RakibHossain621@users.noreply.github.com>
2024-05-28 12:37:52 +0000

    push

---
+++ basic/if.js
@@ -0,0 +1,25 @@
+a = 20
+b = 30
+if(a < b){
+console.log("A is greater then B ")
+}
+if(a > b){
+    console.log("B greter then A")
+}
+
+var n = 5
+if(n % 2 === 0){
+    console.log(n + " is even number")
+}
+if(n % 2 === 1){
+    console.log(n + " is odd number ")
+}
+var age = 24
+var age1 = 26
+if(age<age1){
+    console.log("age is big age1")
+}
+if(age>age1){
+    console.log("age1 is big age")
+}
+
