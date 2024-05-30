26e770f6b1ea0c366527cc5da38f37d21645ab8f
RakibHossain621 <117344777+RakibHossain621@users.noreply.github.com>
2024-05-30 11:41:19 +0000

    push

---
+++ basic/switch.js
@@ -0,0 +1,73 @@
+// var date = new Date()
+
+// var today = date.getDate()
+
+// switch(today){
+//     case 0:
+//     console.log("Today is sunday")
+//     break
+//     case 1:
+//     console.log("Today is monday")
+//     break
+//     case 2:
+//     console.log("Today is Tuesday")
+//     break
+//     case 3:
+//     console.log("Today is wednesday")
+//     break
+//     case 4:
+//     console.log("Today is Thursday")
+//     break
+//     case 5:
+//     console.log("Today is Friday")
+//     break
+//     case 6:
+//     console.log("Today is saturday")
+//     break
+// }
+
+
+var date = new Date()
+
+var today = date.getMonth()
+
+switch(today){
+    case 0:
+    console.log("Today is January")
+    break
+    case 1:
+    console.log("Today is Feb")
+    break
+    case 2:
+    console.log("Today is March")
+    break
+    case 3:
+    console.log("Today is April")
+    break
+    case 4:
+    console.log("Today is May")
+    break
+    case 5:
+    console.log("Today is June")
+    break
+    case 6:
+    console.log("Today is July")
+    break
+    case 7:
+    console.log("Today is August")
+    break
+    case 8:
+    console.log("Today is Septmeber")
+    break
+    case 9:
+    console.log("Today is october")
+    break
+    case 10:
+    console.log("Today is November ")
+    break
+    case 11:
+    console.log("Today is Decmber")
+    break
+    
+    
+}
