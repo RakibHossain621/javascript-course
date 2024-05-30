de7a23637bff21cd91f1b1c539c6301eb2196aa3
RakibHossain621 <117344777+RakibHossain621@users.noreply.github.com>
2024-05-30 10:38:59 +0000

    push

---
+++ basic/else.js
@@ -0,0 +1,8 @@
+var n = 50
+if (n ===0){
+    console.log(n+ "is 49")
+}else if(n % 2 === 0){
+    console.log(n +" is even number")
+}else{
+    console.log(n + " is odd number ")
+}
--- basic/if.js
+++ basic/if.js
@@ -1,25 +1,47 @@
-a = 20
-b = 30
-if(a < b){
-console.log("A is greater then B ")
-}
-if(a > b){
-    console.log("B greter then A")
-}
+// a = 20
+// b = 30
+// if(a < b){
+// console.log("A is greater then B ")
+// }
+// if(a > b){
+//     console.log("B greter then A")
+// }
 
-var n = 5
-if(n % 2 === 0){
-    console.log(n + " is even number")
-}
-if(n % 2 === 1){
-    console.log(n + " is odd number ")
-}
-var age = 24
-var age1 = 26
-if(age<age1){
-    console.log("age is big age1")
-}
-if(age>age1){
-    console.log("age1 is big age")
-}
+// var n = 5
+// if(n % 2 === 0){
+//     console.log(n + " is even number")
+// }
+// if(n % 2 === 1){
+//     console.log(n + " is odd number ")
+// }
+// var age = 24
+// var age1 = 26
+// if(age<age1){
+//     console.log("age is big age1")
+// }
+// if(age>age1){
+//     console.log("age1 is big age")
+// }
+
+
+// function getGrade(score){
+//     var grade = "";
+    
+//     if(score > 25 && score <= 30){
+//         grade = "A"
+//     }else if(score > 20 && score <= 25){
+//         grade = "B";
+//     }else if(score > 15 && score <= 20){
+//         grade = "C"
+//     }else if(score > 10 && score <= 15){
+//         grade = "D"
+//     }else if(score > 5 && score <= 10){
+//         grade = "E";
+//     }else{
+//         grade = "F";
+//     }
+    
+//     return grade;
+// }
+
 
